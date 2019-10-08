5decc3c1045e66721a4d864d0a742e05314c28cf
Frank Meerkötter <frank.meerkoetter@basyskom.com>
2019-10-08 22:43:37 +0200

    Add a \sa for the signals emit after connectToEndpoint/disconnect
    Change-Id: Ic522f85c71f07e155f156136dfed111086aa0e3f
    Reviewed-by: Rainer Keller <Rainer.Keller@qt.io>
    (cherry picked from commit 295dd4f28c142a5fbf02fe64604e971c5c787fde)

--- src/opcua/client/qopcuaclient.cpp
+++ src/opcua/client/qopcuaclient.cpp
@@ -373,7 +373,7 @@ QOpcUaPkiConfiguration QOpcUaClient::pkiConfiguration() const
     client->setAuthenticationInformation(authInfo);
     \endcode
 
-    \sa setAuthenticationInformation(), QOpcUaEndpointDescription
+    \sa connected(), stateChanged(), setAuthenticationInformation(), QOpcUaEndpointDescription
 */
 void QOpcUaClient::connectToEndpoint(const QOpcUaEndpointDescription &endpoint)
 {
@@ -383,7 +383,7 @@ void QOpcUaClient::connectToEndpoint(const QOpcUaEndpointDescription &endpoint)
 
 /*!
     Disconnects from the server.
-    \sa connectToEndpoint()
+    \sa disconnected(), connectToEndpoint()
 */
 void QOpcUaClient::disconnectFromEndpoint()
 {
